e52cfd7f0761e33046113597fcced1838fa6ca60
ducmanhtt5 <c9hai120@gmail.com>
2022-04-23 15:43:04 +0700

    a

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e9b2ff3adeb69758d43189cc96f3beed007812ab',
+        'name' => 'ducmanhtt5/mypackage',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ducmanhtt5/mypackage' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e9b2ff3adeb69758d43189cc96f3beed007812ab',
+            'dev_requirement' => false,
+        ),
+    ),
+);
